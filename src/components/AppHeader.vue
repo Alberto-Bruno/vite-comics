e3af7c3f0638e718badb73da77352d467abfae08
Alberto-Bruno <brunoalberto97@outlook.it>
2023-01-21 16:51:41 +0100

    Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -68,7 +68,7 @@ export default {
             <ul>
                 <li v-for="(item, index) in menu" :key="index" :class="item.active ? 'active' : ''">
                     <a :href="item.url">
-                        {{ item.label }}
+                        <strong>{{ item.label }}</strong>
                     </a>
                 </li>
             </ul>
@@ -78,6 +78,13 @@ export default {
 
 
 <style lang="scss">
+@use '../styles/variables.scss' as*;
+
+.logo {
+    width: 90px;
+    margin-right: 100px;
+}
+
 header {
     height: 250px;
     margin: 15px;
@@ -98,11 +105,29 @@ header {
             align-items: center;
 
             li {
-                height: 100%;
+                height: 50%;
                 padding: 1rem;
-                font-size: 13px;
-                font-family: 'Roboto', sans-serif;
+                font-size: 10px;
+                font-family: Roboto, sans-serif;
                 font-weight: 500;
+                display: flex;
+                align-items: center;
+
+                &.active {
+                    border-bottom: $border-bottom;
+
+                    a {
+                        color: $color-blue;
+                    }
+                }
+
+                &:hover {
+                    border-bottom: $border-bottom;
+                }
+
+                a:hover {
+                    color: $color-blue;
+                }
             }
         }
 
