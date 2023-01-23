61ed5c12e659cb7e23b196332bc8a86c557d4f10
Alberto-Bruno <brunoalberto97@outlook.it>
2023-01-23 23:39:22 +0100

    ComicSeries

---
+++ src/components/ComicSeries.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    props: {
+        comic: Object,
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="carde-image">
+            <img :src="series.thumb" alt="">
+        </div>
+        <div class="card-title">
+            {{ comic.series }}
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+
+</style>
